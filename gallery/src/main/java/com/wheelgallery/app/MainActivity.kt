556e61175ce397566795f0a7763f982678a7ea2a
buildwclaude <306704638+buildwclaude@users.noreply.github.com>
2026-07-21 00:11:39 +0545

    Fix invalid weight import

--- gallery/src/main/java/com/wheelgallery/app/MainActivity.kt
+++ gallery/src/main/java/com/wheelgallery/app/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
